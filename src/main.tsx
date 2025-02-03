7086244286cd4859f378b434a0b97b4dcb5d9b68
timurfajzullin <144335476+timurfajzullin@users.noreply.github.com>
2025-02-04 00:19:43 +0300

    update

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import {createRoot} from 'react-dom/client'
-import './index.module.scss'
-import "./Components/adaptive.module.scss"
-import {App} from './Components/App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-    <App/>,
-)
